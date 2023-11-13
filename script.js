40e3738871244013f971ccf4f2d8253bccb31edd
Phili0916 <79313259+Phili0916@users.noreply.github.com>
2023-11-13 22:59:44 +0100

    Working on adding javascript dynamically

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const url =" https://pokeapi.co/api/v2/pokemon/"
 const card = document.getElementById("card")
 const generateBtn = document.getElementById("generate-btn")
+const hitPoints = document.querySelector(".hit-points")
 
 const getPokemonData = async () => {
     const pokemonId = Math.floor(Math.random() * 150) +1
@@ -15,17 +16,29 @@ const getPokemonData = async () => {
 }
 
 const generatePokeCard = (data) => {
-    console.log("data", data)
-    const hp = data.stats[0].base_stat;
-    console.log("hp",hp)
-    const imgSrc = data.sprites.other.dream_world.front_default
-    const pokemonName = data.species.name
+    const hpElement = document.createElement("p")
+    let totalHP = data.stats[0].base_stat;
+    hpElement.textContent = `HP ${totalHP}`;
+    hitPoints.append(hpElement)
+    hitPoints.classList.add("hit-points");
+
+    const imgSrcElement = document.createElement("img")
+    imgSrcElement.src = data.sprites.other.dream_world.front_default
+    document.appendChild(imgSrcElement)
+    imgSrcElement.setAttribute("id", "pokemonImage")
+
+    const pokemonName = data.species.name;
     console.log("pokemonName: ", pokemonName)
-    const type1 = data.types[0].type.name
+    const type1 = data.types[0].type.name;
     // console.log(type1, 'type1')
-    const type2 = data.types[1].type.name
+    const type2 = data.types[1].type.name;
     // console.log(type2, 'type2')
     const statAttack = data.stats[1].base_stat;
+    console.log('statAttack:',statAttack)
+    const statDefence = data.stats[2].base_stat;
+    console.log('statDefence', statDefence)
+    const statSpeed = data.stats[5].base_stat;
+    console.log('statSpeed', statSpeed)
 
 
     if (!type2)  {
